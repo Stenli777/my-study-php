65743e7c807044e6874fd584e92767575ecbfdb4
Stenli777 <alekseysten777@yandex.ru>
2022-09-18 12:51:45 +0300

    edit project

--- OOP/pointers.php
+++ OOP/pointers.php
@@ -8,15 +8,5 @@ class Point
 $point1 = New Point();
 $point1->x = 3;
 $point1->y = 5;
-$point2 = New Point();
-
-function dup($point1)
-{
-
-
-    $point2 = $point1;
-    return $point2;
-}
 
 print_r($point1);
-print_r(dup($point1));
